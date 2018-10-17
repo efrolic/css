226a7e2b6e2724d62185ad03d34c20a7c7e82891
Efrain Peralta <efra23peralta@gmail.com>
2018-10-16 21:56:19 -0400

    Changes  / general
    Cambios en los drops { Se elimino el :after
    
    Se reorganizo el codigo y se agrego Align Page.

--- gulpfile.js
+++ gulpfile.js
@@ -28,8 +28,8 @@ gulp.task('cssmin', ()=> {
     gulp.src('./assets/css/efrolic.css')
         .pipe(cssmin())
         .pipe(autoprefixer({
-          browsers: ['last 2 versions'],
-          cascade: false
+          browsers: ['last 1 versions'],
+          cascade: true
           }))
         .pipe(gulp.dest(data.css));
 });
